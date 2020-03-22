0e0ee80e1eb091799f6aa837dbabcde4ee1e68a7
Leo Gervilla <leo.gervilla@getweave.com@Leos-MacBook-Pro-242.local>
2020-03-21 22:07:00 -0600

    game is functioning

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,14 +2,126 @@ $(document).ready(() => {
     
     
 //program generates a random number between 19 and 120
-    let randomNumber = Math.floor(Math.random() * ((120 - 19) + 1) + 19);
-    $("#randomNumber").html(randomNumber);
+    var randomNumber = Math.floor(Math.random() * 120 + 19);
+    $("#randomNumbers").text(randomNumber);
 
 //Each crystal should have a random hidden value between 1 - 12.
-    let button1 = Math.floor(Math.random() * 12) + 1;
-    let button2 = Math.floor(Math.random() * 12) + 1;
-    let button3 = Math.floor(Math.random() * 12) + 1;
-    let button4 = Math.floor(Math.random() * 12) + 1;
+    var button1 = Math.floor(Math.random() * 12) + 1;
+    var button2 = Math.floor(Math.random() * 12) + 1;
+    var button3 = Math.floor(Math.random() * 12) + 1;
+    var button4 = Math.floor(Math.random() * 12) + 1;
+ //player clicks the crystals to display value to score
+    var total = 0;
+    var wins = 0;
+    var losses = 0;
+    var totScore = 0;
+   
+    $("#totScore").text(totScore);
+    $("#numWins").text(wins);
+    $("#numLosses").text(losses);
+
+//reset function
+
+function reset () {
+    
+    var button1 = Math.floor(Math.random() * 12) + 1;
+    var button2 = Math.floor(Math.random() * 12) + 1;
+    var button3 = Math.floor(Math.random() * 12) + 1;
+    var button4 = Math.floor(Math.random() * 12) + 1;
+    var randomNumber = Math.floor(Math.random() * 120 + 19);
+
+    total = 0;
+    
+    $("#totScore").text(total);
+}
+
+function winner () {
+    alert("Winner, Winner, Chicken Dinner!");
+    wins++;
+    $("#numWins").text(wins);
+    reset();
+}
+
+function loser () {
+    alert("Oh no you've lost to Jumanji!");
+    losses++;
+    $("#numLosses").text(losses);
+    reset();
+}
+
+$("#button1").on("click", function() {
+
+    total = total + button1;
+    $("#totScore").text(total);
+
+    if (total == randomNumber) {
+        winner();
+        document.getElementById("numWins").textContent = wins;
+        reset();
+    }  else if (total > randomNumber) {
+        loser();
+        document.getElementById("numLosses").textContent = losses;
+        reset();
+    }
+
+
+});
+
+$("#button2").on("click", function() {
+
+    total = total + button2;
+    $("#totScore").text(total);
+
+    if (total == randomNumber) {
+        winner();
+        document.getElementById("numWins").textContent = wins;
+        reset();
+    }  else if (total > randomNumber) {
+        loser();
+        document.getElementById("numLosses").textContent = losses;
+        reset();
+    }
+
+
+});
+
+$("#button3").on("click", function() {
+
+    total = total + button3;
+    $("#totScore").text(total);
+
+    if (total == randomNumber) {
+        winner();
+        document.getElementById("numWins").textContent = wins;
+        reset();
+    }  else if (total > randomNumber) {
+        loser();
+        document.getElementById("numLosses").textContent = losses;
+        reset();
+    }
+
+
+});
+
+$("#button4").on("click", function() {
+
+    total = total + button4;
+    $("#totScore").text(total);
+
+    if (total == randomNumber) {
+        winner();
+        document.getElementById("numWins").textContent = wins;
+        reset();
+    }  else if (total > randomNumber) {
+        loser();
+        document.getElementById("numLosses").textContent = losses;
+        reset();
+    }
+
+
+});
+
+
 
 
 
